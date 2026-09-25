c086c8dbbf73c12dc48c5bf2d7ad02fa02b9291a
Emmanuel <emlopezdev@gmail.com>
2026-09-25 12:27:24 -0400

    updated shallow copy notes with nested object spread operator

--- cloning/shallow_copy.js
+++ cloning/shallow_copy.js
@@ -47,3 +47,116 @@ As a one liner simple remember: The spread operator isn't deciding "copy primiti
 objects." It copies the value of each top-level property. When that value happens to be an object
 reference, the reference is what gets copied.
 */
+
+/*
+Here a few ways to create shallow copies for objects and array
+*/
+const object = { id: 1 };
+/*Spread syntax: */ const object2 = { ...object };
+/*Object.assign() (older method): */ const object3 = Object.assign({}, object);
+
+const array = [1, 2, 3, 4];
+/*Spread syntax: */ const array2 = [...array];
+/*Array.from(): */ const array3 = Array.from(array);
+/* slice() */ const array4 = array.slice();
+
+/*
+One neat way to use the spread operator is in combination with a mutation/update
+*/
+
+const user = {
+    name: "John",
+    age: 30,
+};
+
+const updatedUser = {
+    ...user,
+    age: 31,
+};
+
+const numbers = [1, 2, 3, 4];
+
+const updatedNumbers = [...numbers, 4, 5, 6];
+/* here we are spreading all the data from user into updatedUser but at the same time updating or
+technically overriding the age value to 31. For this to work however order matters, when properties
+have the same key, the later value wins. If a new key:value pair is being added then it can go either
+before or after the spread, but always to be safe the spread should happen first. For arrays it will
+just determine the order of values in the array.*/
+
+const updatedUser2 = {
+    age: 31,
+    ...user, // the incoming user data will override the age to 30
+};
+
+const updatedUser3 = {
+    email: "John@email.com",
+    ...user, // this works just fine because user doest't have an email key
+};
+
+const updatedNumbers2 = [...numbers, 4, 5, 6]; // [1, 2, 3, 4, 5, 6]
+const updatedNumbers3 = [4, 5, 6, ...numbers]; // [4, 5, 6, 1, 2, 3]
+
+/*
+One call out which is more common in React, but can be seen in vanilla JS;
+
+Updating nested state or objects with spread
+*/
+
+const [currentUser, setCurrentUser] = useState({
+    name: "John",
+    address: {
+        city: "New York",
+        zip: "10001",
+    },
+});
+/*
+Suppose we wanted to change the city to "Los Angeles", we don't want to mutate the existing state by
+doing something like: currentUser.address.city = "Boston". This will not update the state or trigger
+a re-render because the state is still referencing ths same object. React uses Object.is when
+deciding whether a state update represents a changed value. If you mutate the existing object and
+pass that same object back, React may bail out because the reference hasn't changed.
+*/
+
+// Wrong way
+currentUser.address.city = "Boston";
+
+/*
+end result
+{   <------------------------------- same currentUser object
+    name: "John",
+
+    address: {  <---------------- same address object
+        city: "Boston", <--- new city value
+        zip: "10001"
+    }
+}
+*/
+
+// Correct way
+setCurrentUser({
+    ...currentUser,
+
+    address: {
+        ...currentUser.address,
+        city: "Boston",
+    },
+});
+
+/*
+end result
+{   <------------------------------- new currentUser object
+    name: "John",
+
+    address: {  <---------------- new address object
+        city: "Boston", <--- new city value
+        zip: "10001"
+    }
+}
+*/
+
+/*
+In the correct way, we are updating nested state using the spread operator. We are essentially
+creating a new currentUser object with the currentUser data and updating the address value with a
+new object as well. This will cause react to update the state value wherever it is used because it
+sees a new object being referenced.
+*/
